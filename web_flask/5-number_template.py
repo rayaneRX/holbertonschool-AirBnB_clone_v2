e7a4b9b1718cefec73d1ca45b4a911ef3cb044eb
rayaneRX <elkerayane@gmail.com>
2023-04-03 16:40:35 +0200

    fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,7 +35,7 @@ def number(n):
 
 @app.route("/number_template/<int:n>")
 def number_template(n):
-    return render_template("5-number.html", number=n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == '__main__':
