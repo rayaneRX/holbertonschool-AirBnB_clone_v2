b5c2ac24d7740294b6d05573e749384d7ee93c52
rayaneRX <elkerayane@gmail.com>
2023-04-03 16:10:03 +0200

    add the task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Flask hello world"""
+"""Flask"""
 
 from flask import Flask
 
@@ -21,15 +21,10 @@ def hbnb():
 def C_text(text):
     return "C {}".format(text.replace("_", " "))
 
-
+@app.route('/python/', defaults={'text': 'is cool'})
 @app.route("/python/<text>")
 def python_text(text):
     return "Python {}".format(text.replace("_", " "))
-
-
-@app.route("/python/")
-def python_notext():
-    return "Python {no_text}".format(no_text="is cool")
 
 
 if __name__ == '__main__':
