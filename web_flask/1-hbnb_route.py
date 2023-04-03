97697611005340e0ac210142d11eaeb3df1b853d
rayaneRX <elkerayane@gmail.com>
2023-04-03 15:18:00 +0200

    add the task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,8 +10,12 @@ app.url_map.strict_slashes = False
 @app.route("/")
 def hello_world():
     return "Hello HBNB!"
+
+
 @app.route("/hbnb")
 def hbnb():
     return "HBNB"
+
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
