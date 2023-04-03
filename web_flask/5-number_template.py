eeb5c942e0722c4a5445d7f8ba559a60dd1eb335
rayaneRX <elkerayane@gmail.com>
2023-04-03 16:37:55 +0200

    ass the task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Flask hello world"""
+"""Flask"""
 
 from flask import Flask, render_template
 
@@ -22,24 +22,20 @@ def C_text(text):
     return "C {}".format(text.replace("_", " "))
 
 
+@app.route('/python/', defaults={'text': 'is cool'})
 @app.route("/python/<text>")
 def python_text(text):
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route("/python/")
-def python_notext():
-    return "Python {no_text}".format(no_text="is cool")
-
-
 @app.route("/number/<int:n>")
-def is_number(n):
+def number(n):
     return ("{} is a number".format(n))
 
 
 @app.route("/number_template/<int:n>")
-def is_number_template(n):
-    return render_template("5-number.html", n=n)
+def number_template(n):
+    return render_template("5-number.html", number=n)
 
 
 if __name__ == '__main__':
