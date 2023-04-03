ff840177c98180d66e7a14dd260f7b6d1d06a0ab
rayaneRX <elkerayane@gmail.com>
2023-04-03 16:52:42 +0200

    add the task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Flask hello world"""
+"""Flask"""
 
 from flask import Flask, render_template
 
@@ -22,32 +22,28 @@ def C_text(text):
     return "C {}".format(text.replace("_", " "))
 
 
+@app.route('/python/', defaults={'text': 'is cool'})
 @app.route("/python/<text>")
 def python_text(text):
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route("/python/")
-def python_notext():
-    return "Python {no_text}".format(no_text="is cool")
-
-
 @app.route("/number/<int:n>")
-def is_number(n):
+def number(n):
     return ("{} is a number".format(n))
 
 
 @app.route("/number_template/<int:n>")
-def is_number_template(n):
+def number_template(n):
     return render_template("5-number.html", n=n)
 
 
 @app.route("/number_odd_or_even/<int:n>")
-def odd_or_even(n):
+def number_odd_or_even(n):
     if n % 2 == 0:
-        return render_template("6-number_odd_or_even.html", n=n, parity='even')
+        return render_template("6-number_odd_or_even.html", n=n, odd_or_even="even")
     else:
-        return render_template("6-number_odd_or_even.html", n=n, parity='odd')
+        return render_template("6-number_odd_or_even.html", n=n, odd_or_even="odd")
 
 
 if __name__ == '__main__':
