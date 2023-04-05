6e3a7b2ed46461ee4f1f87cf46d94db2fbd88376
rayaneRX <elkerayane@gmail.com>
2023-04-05 16:04:55 +0200

    fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Flask"""
-
+"""
+Script that starts a Flask web application
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -11,16 +12,16 @@ app = Flask(__name__)
 
 @app.route("/states", defaults={'id': None}, strict_slashes=False)
 @app.route("/states/<id>", strict_slashes=False)
-def states(id):
+def states_list(id):
     states = storage.all(State)
     if id:
         id = 'State.' + id
-    return render_template("9-states.html", states=states, id=id)
+    return render_template('9-states.html', states=states, id=id)
 
 
 @app.teardown_appcontext
-def teardown_handle(app):
-    storage.close()
+def close_db(exception):
+   storage.close()
 
 
 if __name__ == '__main__':
